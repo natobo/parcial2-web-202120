be3f846e031e8bc132d7fe573726044299d0e88b
natobo <na.tobo@uniandes.edu.co>
2021-12-03 21:49:07 -0500

    puntos 1,2,3,4 y 5 completos

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,11 @@
 const data = require('../assets/data');
 
 function getProducts(query) {
-  return data;
+  let resData = data;
+  if(query){
+    resData = resData.filter(p => p.name.toLowerCase().includes(query.toLowerCase()));
+  }
+  return resData;
 }
 
 module.exports = { getProducts };
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const productController = require('../controllers/product');
 
 /* GET products listing. Please establish connection with getProduct function from controllers/product.js  */
 router.get('/', function (req, res, next) {
-  res.send(
-    'To implement. Please establish connection with getProduct function from controllers/product.js'
-  );
+  var query = req.query.q;
+  const response =  productController.getProducts(query);
+  res.send(response);  
 });
 
 module.exports = router;
